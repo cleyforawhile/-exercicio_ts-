61147207c01f9f13ef191d3f2e08e9daddac7a9b
Hal <hal@hotmail.com>
2024-08-09 13:35:36 -0300

    primeiro commit

---
+++ Source/exercicio.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicacao(a, b) {
+    return a * b;
+}
+let resultado = multiplicacao(2, 5);
+console.log(resultado);
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+let mensagem = saudacao("Halcer");
+console.log(mensagem);
---
+++ Source/exercicio.ts
@@ -0,0 +1,13 @@
+function multiplicacao(a: number, b: number): number {
+    return a * b
+}
+
+let resultado = multiplicacao(2,5);
+console.log(resultado);
+
+function saudacao(nome: string) {
+    return "Olá " + nome
+}
+
+let mensagem = saudacao("Halcer")
+console.log(mensagem)
